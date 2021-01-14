947445832de500469dcf55004e801b65d389b70a
Sudhansh Kesharwani <skeshu28@gmail.com>
2021-01-14 21:25:55 +0530

    SPA2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, Media } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody,CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';	
 	
-	function RenderDish({dish}){
+function RenderDish({dish}){
 		return(
 			<Card>
 				<CardImg width="100%" object src={dish.image} alt={dish.name} />
@@ -43,18 +44,29 @@ import { Card, CardImg, CardText, CardBody, CardTitle, Media } from 'reactstrap'
 
 	const  DishDetail = (props) => {
 		if (props.dish != null) {
-			return(
-				<div className="container">
-                    <div className="row">
-                        <div className="col-12 col-md-5 m-1">
-                            <RenderDish dish={props.dish} />
-                        </div>
-                        <div className="col-12 col-md-5 m-1">
-                            <RenderComments comments={props.dish.comments} />
-                        </div>
+			return (
+                <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>                
+                </div>
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderDish dish={props.dish} />
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderComments comments={props.comments} />
                     </div>
                 </div>
-			);
+                </div>
+            );
 		}
 		else {
 			return(
